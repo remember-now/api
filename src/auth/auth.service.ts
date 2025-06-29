6f29d8c3a4a38ceb644a3d06800ba68de8be2b01
jake158 <kulikovpavel2801@gmail.com>
2025-06-28 19:36:22 -0700

    feat: add basic prisma request validation, auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          passwordHash: hash,
+        },
+      });
+
+      return {
+        id: user.id,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      console.error('Signup error:', error);
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Invalid credentials');
+
+    const pwMatches = await argon.verify(user.passwordHash, dto.password);
+    if (!pwMatches) throw new ForbiddenException('Invalid credentials');
+
+    return {
+      id: user.id,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    };
+  }
 }
