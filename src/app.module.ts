f46ecf264d5dbe5656f2e48901bc5cac6aba3602
jake158 <kulikovpavel2801@gmail.com>
2025-06-29 14:27:19 -0700

    feat: use nest config module instead of process.env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
 })
 export class AppModule {}
