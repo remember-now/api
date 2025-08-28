96c7000e9180ace00baba31561af56ac5819026b
jake158 <kulikovpavel2801@gmail.com>
2025-08-27 20:45:15 -0700

    feat: add basic CORS support for dev server

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(Logger);
 
+  app.enableCors({
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3333);
   logger.log(`Application listening at ${await app.getUrl()}`);
 }
